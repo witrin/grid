98634dac3e8a4ff0c5a90ff9d436f8cf35229eaa
Artus Kolanowski <artus@ionoi.net>
2017-11-20 20:07:05 +0100

    [TASK] Page layout module description is gone

--- ext_tables.php
+++ ext_tables.php
@@ -11,7 +11,7 @@ defined('TYPO3_MODE') or die();
         'access' => 'user,group',
         'icon' => 'EXT:backend/Resources/Public/Icons/module-page.svg',
         'name' => \TYPO3\CMS\Grid\Controller\PageLayoutController::MODULE_NAMESPACE . '_' . \TYPO3\CMS\Grid\Controller\PageLayoutController::MODULE_NAME,
-        'labels' => 'LLL:EXT:grid/Resources/Private/Language/ext_tables:module.layout.title'
+        'labels' => 'LLL:EXT:grid/Resources/Private/Language/locallang_mod.xlf'
     ]
 );
 
